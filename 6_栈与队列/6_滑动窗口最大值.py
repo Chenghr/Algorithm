5733c62711c32ec970932b8be8ee8e00caeace02
mac <1329753495@qq.com>
2022-01-03 19:20:07 +0800

    to be rewrite and add some blog link

--- 6_栈与队列/6_滑动窗口最大值.py
+++ 6_栈与队列/6_滑动窗口最大值.py
@@ -40,6 +40,7 @@
                 采用 collections.deque 实现;
 
                 一些关于时间复杂的分析的博客:
+                https://www.cnblogs.com/Nicholas0707/p/13040246.html
                 https://blog.csdn.net/qq_28304687/article/details/79088491
 """
 
