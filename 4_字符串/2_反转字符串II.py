f54ce752fbbdc1d39fc5572ca6c7c1d5113da45e
mac <1329753495@qq.com>
2022-03-06 16:58:44 +0800

    ac

--- 4_字符串/2_反转字符串II.py
+++ 4_字符串/2_反转字符串II.py
@@ -1,19 +1,26 @@
 """
-给定一个字符串 s 和一个整数 k，从字符串开头算起，
-    每计数至 2k 个字符，就反转这 2k 字符中的前 k 个字符。
-    如果剩余字符少于 k 个，则将剩余字符全部反转。
-    如果剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符，其余字符保持原样。
+    题目描述:
+        给定一个字符串 s 和一个整数 k，从字符串开头算起，
+            每计数至 2k 个字符，就反转这 2k 字符中的前 k 个字符。
+            如果剩余字符少于 k 个，则将剩余字符全部反转。
+            如果剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符，其余字符保持原样。
 
-提示：
-    1 <= s.length <= 104
-    s 仅由小写英文组成
-    1 <= k <= 104
+        提示:
+            1 <= s.length <= 104
+            s 仅由小写英文组成
+            1 <= k <= 104
 
-https://leetcode-cn.com/problems/reverse-string-ii/
+    链接: https://leetcode-cn.com/problems/reverse-string-ii/
 """
 
 class Solution:
     def reverseStr(self, s: str, k: int) -> str:
+        """
+            1. 选中前 2k 个字符或者剩下所有字符；
+            2. 对选中的 2k 个字符中前 k 个字符反转，或者反转选中所有字符；
+
+            对上述流程进行合并。
+        """
         index, s = 0, list(s)
 
         while (index < len(s)):
