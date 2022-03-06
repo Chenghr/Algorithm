a43e823681d88dd0e087416c7c8678fc5996fe93
mac <1329753495@qq.com>
2022-03-06 16:58:59 +0800

    kmp算法，需要反复斟酌

--- 4_字符串/6_实现 strStr().py
+++ 4_字符串/6_实现 strStr().py
@@ -1,24 +1,25 @@
-"""题目描述:
+"""
+    题目描述:
+        实现 strStr() 函数。
+        给你两个字符串 haystack 和 needle ，
+        请你在 haystack 字符串中找出 needle 字符串出现的第一个位置（下标从 0 开始）。
+        如果不存在，则返回  -1 。
 
-    实现 strStr() 函数。
-    给你两个字符串 haystack 和 needle ，
-    请你在 haystack 字符串中找出 needle 字符串出现的第一个位置（下标从 0 开始）。
-    如果不存在，则返回  -1 。
+        说明: 
+            当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
+            对于本题而言，当 needle 是空字符串时我们应当返回 0 。
+            这与 C 语言的 strstr() 以及 Java 的 indexOf() 定义相符。
 
-    说明：
-        当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
-        对于本题而言，当 needle 是空字符串时我们应当返回 0 。
-        这与 C 语言的 strstr() 以及 Java 的 indexOf() 定义相符。
+        提示: 
+            0 <= haystack.length, needle.length <= 5 * 104
+            haystack 和 needle 仅由小写英文字符组成
 
-    提示：
-        0 <= haystack.length, needle.length <= 5 * 104
-        haystack 和 needle 仅由小写英文字符组成
-
-    https://leetcode-cn.com/problems/implement-strstr/
+    链接: https://leetcode-cn.com/problems/implement-strstr/
 """
 
-"""思路分析:
-    本题本质上是个字符串匹配问题，有两种实现思路:
+"""
+    思路分析:
+        本题本质上是个字符串匹配问题，有两种实现思路:
 
     1. 暴力匹配，O(M * N) 时间开销；
 
