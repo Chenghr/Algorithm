b3e2b7f7da13838889e9a30a29acda4afa5697e3
mac <1329753495@qq.com>
2022-03-09 10:54:14 +0800

    read again

--- labuladong/chap1_基础数据结构/1.1_数组双指针/1_二分搜索/1_排序数组查找元素的第一个和最后一个位置.py
+++ labuladong/chap1_基础数据结构/1.1_数组双指针/1_二分搜索/1_排序数组查找元素的第一个和最后一个位置.py
@@ -19,13 +19,15 @@ class Solution:
             mid = int((left + right) / 2)
 
             if nums[mid] == target:
-                # 收缩右边界, 这里不能写 mid + 1
+                # 收缩右边界，这里不能写 mid + 1
                 right = mid
             elif nums[mid] < target:
                 left = mid + 1
             elif nums[mid] > target:
                 right = mid
         
+        # left 的取值范围为 [0, len(nums)], len(nums)为越界，需要剔除。
+        # 如果只有一个 target，right = mid 不动，left 会收缩到 = right为止，即 nums[left] == target
         if left == len(nums) or nums[left] != target:
             return -1
 
@@ -46,6 +48,8 @@ class Solution:
             elif nums[mid] > target:
                 right = mid
         
+        # left -1 不能越界，则 left 不可为0， 为 len(nums)可以
+        # 判断 left-1 的值和收缩左边界策略相关
         if left == 0 or nums[left-1] != target:
             return -1
 
